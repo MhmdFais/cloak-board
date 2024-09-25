9734ce9c97569d911d873e348c0d24b84f3dfabc
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-09-25 19:20:41 +0530

    feat: Add session middleware and configuration
    This commit adds session middleware and configuration to the server.js file. It includes the necessary dependencies and sets up the session secret, resave, saveUninitialized, and cookie options.

--- server/server.js
+++ server/server.js
@@ -2,8 +2,17 @@ require('dotenv').config();
 const express = require('express')
 const session = require("express-session")
 const passport = require("passport")
+const LocalStrategy = require('passport-local').Strategy;
+const bycrypt = require('bcryptjs');
 
 const app = express()
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
 
 const port = process.env.PORT || 3000
 
