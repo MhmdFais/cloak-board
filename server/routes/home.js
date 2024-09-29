3bce1b06822a17847b43e5f5d27a81d59244118a
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-09-29 21:29:40 +0530

    Refactor home.js and add Message controller

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const Message = require('../controller/message')
 
 router.get('/', async (req, res) => {
     if (req.isAuthenticated()) {
