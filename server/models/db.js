877929abaa17c7c067e246e894f16b4756ea1d98
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-09-29 21:31:03 +0530

    Refactor database connection URL to use environment variable

--- server/models/db.js
+++ server/models/db.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-//let dbPass = process.env.DB_PASS
+let dbPass = process.env.DB_PASS
 
-//let url = `mongodb+srv://mofam534:${dbPass}.wj6ie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+let url = `mongodb+srv://mofam534:${dbPass}.wj6ie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 const connectToDB = async () => {
     try{
-        const connection = await mongoose.connect('mongodb://localhost:27017/auth',{
+        const connection = await mongoose.connect(url,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
